22c680765ed4804e8e81e5667a0cd933b6f005af
Tomáš Mikula <mail@tomasmikula.cz>
2017-07-17 19:12:40 +0200

    Clean code

--- herocomp/tree/operations/BitwiseOrOperation.py
+++ herocomp/tree/operations/BitwiseOrOperation.py
@@ -1,5 +1,3 @@
-from asm.Asm import *
-from asm.Registers import Registers
 from tree.operations.BinaryOperation import BinaryOperation
 from tree.operations.OperationType import OperationType
 
@@ -14,25 +12,3 @@ class BitwiseOrOperation(BinaryOperation):
         statementsString = self.printStatements()
         return "BitwiseOrOperation {}: {}".format(self.operation,
                                                  statementsString)
-
-    def get_operation_code(self):
-        code = ""
-
-        code += pop(Registers.R10)
-        code += pop(Registers.R11)
-
-        code += cmp("$0", Registers.R10)
-        # code += movq("$1", Registers.R10)
-        code += movq("$0", Registers.R12)
-        code += cmove(Registers.R12, Registers.R10)
-
-        code += cmp("$0", Registers.R11)
-        # code += cmp("$1", Registers.R11)
-        code += movq("$0", Registers.R12)
-        code += cmove(Registers.R12, Registers.R11)
-
-        code += orq(Registers.R10, Registers.R11)
-
-        code += push(Registers.R11)
-
-        return code
